4d3d08666ba409c4114ecbbdd9127e2e249495cc
Rodrigo Diaz Tajada <r2d2rigo@gmail.com>
2017-08-14 12:21:45 +0100

    Tagged Tile class so it can be JSON serialized.

--- GMSharp/Tile.cs
+++ GMSharp/Tile.cs
@@ -1,51 +1,67 @@
-﻿using System.Xml;
+﻿using Newtonsoft.Json;
+using System.Xml;
 using System.Xml.Serialization;
 
 namespace GMSharp
 {
     [XmlType("tile")]
+    [JsonObject]
     public class Tile
     {
         [XmlAttribute("bgName")]
+        [JsonProperty("bgName")]
         public string BackgroundName { get; set; }
 
         [XmlAttribute("x")]
+        [JsonProperty("x")]
         public int X { get; set; }
 
         [XmlAttribute("y")]
+        [JsonProperty("y")]
         public int Y { get; set; }
 
         [XmlAttribute("w")]
+        [JsonProperty("w")]
         public int Width { get; set; }
 
         [XmlAttribute("h")]
+        [JsonProperty("h")]
         public int Height { get; set; }
 
         [XmlAttribute("xo")]
+        [JsonProperty("xo")]
         public int XOrigin { get; set; }
 
         [XmlAttribute("yo")]
+        [JsonProperty("yo")]
         public int YOrigin { get; set; }
 
         [XmlAttribute("id")]
+        [JsonProperty("id")]
         public int Id { get; set; }
 
         [XmlAttribute("name")]
+        [JsonProperty("name")]
         public string Name { get; set; }
 
         [XmlAttribute("depth")]
+        [JsonProperty("depth")]
         public int Depth { get; set; }
 
         [XmlAttribute("locked")]
+        [JsonProperty("locked")]
         public int IsLocked { get; set; }
 
         [XmlAttribute("colour")]
+        [JsonProperty("colour")]
         public uint Colour { get; set; }
 
         [XmlAttribute("scaleX")]
+        [JsonProperty("scaleX")]
         public double XScale { get; set; }
 
         [XmlAttribute("scaleY")]
+        [JsonProperty("scaleY")]
         public double YScale { get; set; }
 
         public Tile Clone()
