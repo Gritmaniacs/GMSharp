2713cb50acd506a2c2536d0c3c65c79d1ecb8081
Rodrigo Diaz Tajada <r2d2rigo@gmail.com>
2017-08-14 12:26:21 +0100

    Tagged Instance class so it can be JSON serialized.

--- GMSharp/Instance.cs
+++ GMSharp/Instance.cs
@@ -1,38 +1,50 @@
-﻿using System.Xml.Serialization;
+﻿using Newtonsoft.Json;
+using System.Xml.Serialization;
 
 namespace GMSharp
 {
     [XmlType("instance")]
+    [JsonObject]
     public class Instance
     {
         [XmlAttribute("objName")]
+        [JsonProperty("objName")]
         public string ObjectName { get; set; }
 
         [XmlAttribute("x")]
+        [JsonProperty("x")]
         public int X { get; set; }
 
         [XmlAttribute("y")]
+        [JsonProperty("y")]
         public int Y { get; set; }
 
         [XmlAttribute("name")]
+        [JsonProperty("name")]
         public string Name { get; set; }
 
         [XmlAttribute("locked")]
+        [JsonProperty("locked")]
         public int IsLocked { get; set; }
 
         [XmlAttribute("code")]
+        [JsonProperty("code")]
         public string Code { get; set; }
 
         [XmlAttribute("scaleX")]
+        [JsonProperty("scaleX")]
         public double XScale { get; set; }
 
         [XmlAttribute("scaleY")]
+        [JsonProperty("scaleY")]
         public double YScale { get; set; }
 
         [XmlAttribute("colour")]
+        [JsonProperty("colour")]
         public uint Colour { get; set; }
 
         [XmlAttribute("rotation")]
+        [JsonProperty("rotation")]
         public double Rotation { get; set; }
 
         public Instance Clone()
